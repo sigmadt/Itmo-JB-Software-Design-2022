05276ab6458b7eb53dc9f3dd7cd94cce66e1ef6b
D <sayfulindmitry@gmail.com>
2022-06-01 21:20:44 +0300

    reworked pwd command for convenient cd and ls implementation

--- src/ru/itmo/sd/bash/res/commands/PwdCommand.java
+++ src/ru/itmo/sd/bash/res/commands/PwdCommand.java
@@ -8,13 +8,14 @@ import ru.itmo.sd.bash.res.utils.exceptions.WrongSyntaxException;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Path;
 import java.util.List;
 
 public class PwdCommand implements Command {
     @Override
     public InputStream run(InputStream input, List<String> arguments, EnvManager envManager) throws IOException, WrongSyntaxException {
-        var fullPathBytes = Utils
-                .getCurrentDir()
+        var fullPathBytes =
+                Path.of(envManager.get("PWD"))
                 .toAbsolutePath()
                 .toString()
                 .concat("\n")
